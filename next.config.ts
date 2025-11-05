2b5fd1a2a180b108eeaaf43b648b4650e8836404
JebGy <martinmunayco123@hotmail.com>
2025-11-04 22:58:11 -0500

    FEAT: rebrand

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     experimental: {
     optimizePackageImports:["@prisma/client"],
-    // Helpful when using server components; harmless for pages/api
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
   },
   
 };
